9395963ea62e75ec92c71c2bef141fdde7f11e9d
deepaksy <deepaksy28@gmail.com>
2025-07-24 12:36:52 -0700

    Add about & posts page

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -10,6 +10,8 @@ import PublicRoutes from './outlets/PublicRoutes'
 import ProtectedRoute from './outlets/ProtectedRoute'
 import Login from './pages/login'
 import Signup from './pages/signup'
+import Aboutpage from './pages/about'
+import Postspage from './pages/post'
 
 const AppRouter = () => {
   return (
@@ -20,6 +22,8 @@ const AppRouter = () => {
               <Route path='/' element={<App />} />
               <Route path='/login' element={<Login />} />
               <Route path='/signup' element={<Signup />} />
+              <Route path='/about' element={<Aboutpage />} />
+              <Route path='/posts' element={<Postspage />} />
             </Route>
             <Route path='/test' element={<ProtectedRoute />} >
               <Route path='/test' element={<Test />} />
---
+++ frontend/src/pages/post/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Postspage = () => {
+  return (
+    <div>Postspage</div>
+  )
+}
+
+export default Postspage
