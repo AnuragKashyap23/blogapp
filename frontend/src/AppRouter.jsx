9ed4cba520d7c8ab574a915d3499130c4f49798c
deepaksy <deepaksy28@gmail.com>
2025-07-24 12:33:47 -0700

    Add login & signup page

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -8,6 +8,8 @@ import Navbar from './components/Navbar'
 import Test from './pages/test'
 import PublicRoutes from './outlets/PublicRoutes'
 import ProtectedRoute from './outlets/ProtectedRoute'
+import Login from './pages/login'
+import Signup from './pages/signup'
 
 const AppRouter = () => {
   return (
@@ -16,6 +18,8 @@ const AppRouter = () => {
         <Routes>
             <Route  element={<PublicRoutes />} >
               <Route path='/' element={<App />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
             </Route>
             <Route path='/test' element={<ProtectedRoute />} >
               <Route path='/test' element={<Test />} />
