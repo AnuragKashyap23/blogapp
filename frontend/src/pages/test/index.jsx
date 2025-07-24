c44a125fad14ebec855e653de690297ac54711b8
deepaksy <deepaksy28@gmail.com>
2025-07-24 12:00:09 -0700

    update test page

--- frontend/src/pages/test/index.jsx
+++ frontend/src/pages/test/index.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Test = () => {
   return (
     <div>
-        <p className='text-4xl font-extrabold'>Test</p>
+        <div className='h-20 w-full  bg-amber-200 dark:bg-gray-950'>
+          <p className='text-4xl font-extrabold dark:text-amber-50'>Test</p>
+        </div>
     </div>
   )
 }
