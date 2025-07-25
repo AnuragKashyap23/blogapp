9e49bdceb3d484ed81ca05e1f3095eb8cc39cc03
deepaksy <deepaksy28@gmail.com>
2025-07-24 21:48:00 -0700

    Add login & signup page

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username:'',
+    password:''
+  })
+
+  const handleFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]:[e.target.value]
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`${formData.username} - ${formData.password}`)
+    setFormData({
+      username:'',
+      password:''
+    })
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <div className='flex flex-row justify-center h-20 w-full bg-amber-400'>
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <input name='username' type='text' placeholder='username' value={formData.username}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input name='password' type='password' placeholder='password' value={formData.password}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input type='submit' value='submit' placeholder='submit' />
+        </form>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/signup/index.jsx
+++ frontend/src/pages/signup/index.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstname:'',
+    lastname:'',
+    username:'',
+    password:'',
+    email:''
+  })
+
+  const handleFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]:[e.target.value]
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`${formData.username} - ${formData.password}`)
+    setFormData({
+      username:'',
+      password:'',
+      firstname:'',
+      email:'',
+      lastname:''
+    })
+  }
+
+
   return (
-    <div>Signup</div>
+    <div>
+      <h1>Signup</h1>
+      <div>
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <input name='firstname' type='text' placeholder='firstname' value={formData.firstname}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input name='lastname' type='text' placeholder='lastname' value={formData.lastname}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input name='username' type='text' placeholder='username' value={formData.username}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input name='password' type='password' placeholder='password' value={formData.password}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input name='email' type='email' placeholder='email' value={formData.email}  onChange={(e)=>{handleFormChange(e)}}/>
+          <input type='submit' value='submit' placeholder='submit' />
+        </form>
+      </div>
+    </div>
   )
 }
 
